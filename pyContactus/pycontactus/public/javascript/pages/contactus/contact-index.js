42abf63e7b47a2d836add8533c8b7117c44f6add
job <job@192.168.110.71>
2017-02-24 19:35:15 +0700

    t

---
+++ pyContactus/pycontactus/public/javascript/pages/contactus/contact-index.js
@@ -0,0 +1,73 @@
+var app = angular.module('Contactus', ['ui.grid', 'ngTouch', 'ngAnimate', 'ui-listView', 'pascalprecht.translate', 'ngCookies']);
+	app.controller('ContactusData', function($scope, $http, $translate, i18nService, $window){
+		$window.changLang=function(langKey){
+			console.log('changLang');
+			$translate.use(langKey);
+			$translate.refresh(langKey);
+		};	
+		
+		$scope.init = function(){
+			language.init();
+			
+			
+			
+		}
+		
+		
+		
+		$("#contactus-form").validate({
+			rules:{
+				email: {
+					required: true,
+					minlength: 2
+				},
+				message: {
+					required: true,
+					minlength: 2
+				}
+			},
+			messages: {
+				email :{
+					required: function() { return window.lang.translate('msg_enter_your_email'); } ,
+					minlength: function() {return window.lang.translate('Your username must consist of at least 2 characters'); }
+				},
+				message :{
+					required: function() { return window.lang.translate('msg_enter_your_message'); } ,
+					minlength: function() {return window.lang.translate('Your username must consist of at least 2 characters'); }
+				}
+			},
+			errorElement: 'em',
+			errorPlacement: function ( error, element ) {
+				error.addClass( "help-block" );
+				if ( element.prop( "type" ) === "checkbox" ) {
+					error.insertAfter( element.parent( "label" ) );
+				} else {
+					error.insertAfter( element );
+				}
+			},
+			highlight: function (element, errorClass, validClass) {
+				$( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+            },
+			unhighlight: function (element, errorClass, validClass) {
+				$( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+			}
+		});		
+		
+		
+		$scope.submitContactus = function() {
+	        console.log("posting data....");
+	       
+	        //$http.post('form.php', JSON.stringify(data)).success(function(){/*success callback*/});
+		};
+		
+		$scope.reportDetailTypeData
+		   
+	
+	    
+	});
+	
+	app.config( function($translateProvider){
+		$translateProvider.useUrlLoader('/script/loadLangJquery');	 
+		$translateProvider.preferredLanguage('th');	 
+		$translateProvider.useCookieStorage();
+	});
