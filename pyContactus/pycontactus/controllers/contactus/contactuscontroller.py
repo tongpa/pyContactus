7ea7059cd415818232eb84c565d3aa6fc6d1a56e
job <job@192.168.110.71>
2017-02-27 10:14:47 +0700

    ss

--- pyContactus/pycontactus/controllers/contactus/contactuscontroller.py
+++ pyContactus/pycontactus/controllers/contactus/contactuscontroller.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Main Controller"""
 from tg import TGController
-from tg import expose, flash, require, url, lurl, request, redirect, validate
+from tg import expose, flash, require, url, lurl, request, redirect, validate, config
 from tg.i18n import ugettext as _
 
 from pycontactus import model
@@ -27,29 +27,5 @@ class ContactusController(BaseController):
         
         self.__getMasterData()
 
-    @expose('pycontactus.templates.index')
-    def index(self):
-        reload(sys).setdefaultencoding('utf8') 
-        self.report_type=[]     
-        self.report_type = model.DetailReportType.getAll(1)
-        for r in self.report_type:
-            print "%s %s"%(r.id_detail_report_type, r.active)
-        
-        return dict(page='index', reporttype=self.report_type)
-    @expose()
-    def AddContactUs(self, **kw):
-        reload(sys).setdefaultencoding('utf8') 
-        self.success = True
-        try :
-            self.ContactUs = model.DetailReport();
-            self.ContactUs.id_detail_report_type = 1
-            self.ContactUs.email = kw['email']
-            self.ContactUs.message = kw['message']
-            self.ContactUs.save()
-            if self.ContactUs :
-                self.result = True
-                self.message = 'Add Contact Us Success.'
-        except Exception as e:
-            self.result =False;
-            self.message = '' + str(e); 
+
         return dict(success=self.success, result=self.result, message=self.message)
